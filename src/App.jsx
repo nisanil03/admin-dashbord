b66d3c5315eddbc93467aea4c4f8ce2ff66d9508
Anil jaiswal <147340526+nisanil03@users.noreply.github.com>
2025-02-04 03:14:20 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar";
 import OverviewPage from "./pages/OverviewPage";
-import ProductsPage from "./pages/productsPage";
+import ProductsPage from "./pages/ProductsPage";
 import UsersPage from "./pages/UsersPage";
 import SalesPage from "./pages/SalesPage";
 import OrdersPage from "./pages/OrdersPage";
